a2e9783cb508a5cf46cfab83e3262d294578f5f0
techotaku1 <jsdg1818@gmail.com>
2024-12-20 20:01:29 -0500

    mejoras

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ import "./src/env.js";
 const config = {
   images: {
     unoptimized: true, // Desactiva la optimización de imágenes
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'artiefy-upload.s3.us-east-2.amazonaws.com',
+        pathname: '/**', // Permite todos los archivos del bucket
+      },
+    ],
   },
 };
 
