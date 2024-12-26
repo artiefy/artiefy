c10e1bc7abb5e1b9a793f246489b009fd489207b
techotaku1 <jsdg1818@gmail.com>
2024-12-26 14:21:17 -0500

    ARTIEFY FULL

--- src/components/layout/SkeletonCard.tsx
+++ src/components/layout/SkeletonCard.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "~/components/ui/skeleton"
+import { Skeleton } from "~/components/ui/skeleton";
 
 export function SkeletonCard() {
   return (
@@ -9,6 +9,6 @@ export function SkeletonCard() {
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
-  )
+  );
 }
 
