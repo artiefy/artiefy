cd82b7ff377d9110b92f59625953ca08f51f68ab
techotaku1 <jsdg1818@gmail.com>
2024-12-27 23:42:18 -0500

    implmentacion stable del ratelimit con toast

--- src/server/ratelimit/ratelimit.ts
+++ src/server/ratelimit/ratelimit.ts
@@ -1,12 +1,10 @@
-//src\server\ratelimit\ratelimit.ts
-
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(1, "100 s"),
+  limiter: Ratelimit.slidingWindow(10, "100 s"),
   analytics: true,
   /**
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
