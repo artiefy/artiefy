f3c905723e28e56a2d9592e789dde30c5df15ae3
techotaku1 <jsdg1818@gmail.com>
2024-12-31 01:33:46 -0500

    og

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -3,7 +3,6 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export const alt = 'Artiefy - Aprende y Crea';
 export const size = {
   width: 1200,
   height: 630,
@@ -16,28 +15,57 @@ export default async function Image() {
   const logoSrc = await fetch(new URL('/artiefy-icon.png', baseUrl)).then(
     (res) => res.arrayBuffer()
   );
+  const iconSrc = await fetch(new URL('/icon.png', baseUrl)).then(
+    (res) => res.arrayBuffer()
+  );
 
   return new ImageResponse(
     (
       <div
         style={{
           display: 'flex',
-          alignItems: 'center',
+          flexDirection: 'column',
           justifyContent: 'center',
-          width: '100%',
+          alignItems: 'center',
+          fontSize: 36,
+          fontWeight: 700,
+          textAlign: 'center',
+          padding: 70,
+          color: 'white',
+          backgroundImage: 'linear-gradient(to right, #3AF4EF, #01142B)',
           height: '100%',
-          backgroundColor: '#f0f0f0',
-          fontSize: 48,
-          fontWeight: 'bold',
-          color: '#333',
+          width: '100%'
         }}
       >
-        <img src={`data:image/png;base64,${Buffer.from(logoSrc).toString('base64')}`} height="100" alt="Artiefy Logo" />
-        <span>Artiefy - An Educational Tool for Online Courses</span>
+        <img src={`data:image/png;base64,${Buffer.from(logoSrc).toString('base64')}`} height="150" alt="Artiefy Logo" />
+        <div
+          style={{
+            backgroundClip: 'text',
+            WebkitBackgroundClip: 'text',
+            color: 'white',
+            fontWeight: 'bold',
+            marginTop: 20,
+            fontSize: 48,
+          }}
+        >
+          Artiefy - App Web Educativa de Cursos Online 🎓
+        </div>
+        <div
+          style={{
+            marginTop: 20,
+            fontSize: 28,
+            fontWeight: 'normal',
+          }}
+        >
+          Bienvenido a Artiefy, donde el aprendizaje nunca se detiene. ¡Inspírate y crece con nosotros!
+        </div>
+        <img src={`data:image/png;base64,${Buffer.from(iconSrc).toString('base64')}`} height="70" alt="Icon" />
       </div>
     ),
     {
-      ...size,
+      width: size.width,
+      height: size.height,
+      emoji: 'twemoji'
     }
   );
 }
