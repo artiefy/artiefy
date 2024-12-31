c0de20fc8cc10d29b54f946ffe7fba0682ef7c17
techotaku1 <jsdg1818@gmail.com>
2024-12-30 20:05:57 -0500

    full modal

--- src/components/layout/CourseForm.tsx
+++ src/components/layout/CourseForm.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { useState, type ChangeEvent, useEffect } from "react";
-import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
-import { Progress } from "~/components/ui/progress";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "~/components/ui/dialog";
+import Image from "next/image";
+import { useEffect, useState, type ChangeEvent } from "react";
 import { FiUploadCloud } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import { Progress } from "~/components/ui/progress";
 
 interface CourseFormProps {
   onSubmitAction: (
@@ -15,7 +22,7 @@ interface CourseFormProps {
     file: File | null,
     category: string,
     instructor: string,
-    rating: number
+    rating: number,
   ) => Promise<void>;
   uploading: boolean;
   editingCourseId: string | null;
@@ -34,7 +41,13 @@ interface CourseFormProps {
   onCloseAction: () => void;
 }
 
-export default function CourseForm({ onSubmitAction, uploading, editingCourseId, isOpen, onCloseAction }: CourseFormProps) {
+export default function CourseForm({
+  onSubmitAction,
+  uploading,
+  editingCourseId,
+  isOpen,
+  onCloseAction,
+}: CourseFormProps) {
   const { user } = useUser();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -43,6 +56,7 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
   const [rating, setRating] = useState(0);
   const [file, setFile] = useState<File | null>(null);
   const [fileName, setFileName] = useState<string | null>(null);
+  const [fileSize, setFileSize] = useState<number | null>(null);
   const [progress, setProgress] = useState(0);
   const [isEditing, setIsEditing] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
@@ -52,9 +66,11 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
     if (files?.[0]) {
       setFile(files[0]);
       setFileName(files[0].name);
+      setFileSize(files[0].size);
     } else {
       setFile(null);
       setFileName(null);
+      setFileSize(null);
     }
   };
 
@@ -75,15 +91,24 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
     if (files?.[0]) {
       setFile(files[0]);
       setFileName(files[0].name);
+      setFileSize(files[0].size);
     } else {
       setFile(null);
       setFileName(null);
+      setFileSize(null);
     }
   };
 
   const handleSubmit = async () => {
     setIsEditing(true);
-    await onSubmitAction(title, description, file, category, instructor, rating);
+    await onSubmitAction(
+      title,
+      description,
+      file,
+      category,
+      instructor,
+      rating,
+    );
   };
 
   useEffect(() => {
@@ -107,7 +132,7 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
     if (progress === 100) {
       const timeout = setTimeout(() => {
         setProgress(0);
-      }, 500); 
+      }, 500);
 
       return () => clearTimeout(timeout);
     }
@@ -129,14 +154,18 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
 
   return (
     <Dialog open={isOpen} onOpenChange={onCloseAction}>
-      <DialogContent className="max-w-lg max-h-screen overflow-y-auto">
+      <DialogContent className="max-h-screen max-w-lg overflow-y-auto">
         <DialogHeader>
-          <DialogTitle>{editingCourseId ? "Editar Curso" : "Crear Curso"}</DialogTitle>
+          <DialogTitle>
+            {editingCourseId ? "Editar Curso" : "Crear Curso"}
+          </DialogTitle>
           <DialogDescription>
-            {editingCourseId ? "Edita los detalles del curso" : "Llena los detalles para crear un nuevo curso"}
+            {editingCourseId
+              ? "Edita los detalles del curso"
+              : "Llena los detalles para crear un nuevo curso"}
           </DialogDescription>
         </DialogHeader>
-        <div className="bg-background p-6 rounded-lg shadow-md text-black">
+        <div className="rounded-lg bg-background p-6 text-black shadow-md">
           <input
             type="text"
             placeholder="Título"
@@ -172,7 +201,7 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
             className="mb-4 w-full rounded border border-primary p-2"
           />
           <div
-            className={`border-2 border-dashed rounded-lg p-8 ${isDragging ? "border-blue-500 bg-blue-50" : "border-gray-300 bg-gray-50"} transition-all duration-300 ease-in-out`}
+            className={`rounded-lg border-2 border-dashed p-8 ${isDragging ? "border-blue-500 bg-blue-50" : "border-gray-300 bg-gray-50"} transition-all duration-300 ease-in-out`}
             onDragOver={handleDragOver}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
@@ -185,6 +214,9 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
               <p className="mt-2 text-sm text-gray-500">
                 o haz clic para seleccionar un archivo desde tu computadora
               </p>
+              <p className="mt-1 text-sm text-gray-500">
+                Supports: JPG, PNG, GIF (Max size: 5MB)
+              </p>
               <input
                 type="file"
                 accept="image/*"
@@ -194,7 +226,7 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
               />
               <label
                 htmlFor="file-upload"
-                className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer"
+                className="mt-4 inline-flex cursor-pointer items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               >
                 Seleccionar Archivo
               </label>
@@ -202,45 +234,51 @@ export default function CourseForm({ onSubmitAction, uploading, editingCourseId,
           </div>
           {fileName && (
             <div className="mt-8">
-              <h3 className="text-lg font-medium text-gray-700 mb-4">
+              <h3 className="mb-4 text-lg font-medium text-gray-700">
                 Vista previa de la imagen
               </h3>
-              <div className="relative group rounded-lg overflow-hidden bg-gray-100">
+              <div className="group relative overflow-hidden rounded-lg bg-gray-100">
                 {file && (
                   <Image
                     src={URL.createObjectURL(file)}
                     alt="preview"
                     width={500}
                     height={200}
-                    className="w-full h-48 object-cover"
+                    className="h-48 w-full object-cover"
                   />
                 )}
                 <button
                   onClick={() => {
                     setFile(null);
                     setFileName(null);
+                    setFileSize(null);
                   }}
-                  className="absolute top-2 right-2 p-1 rounded-full bg-red-500 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+                  className="absolute right-2 top-2 rounded-full bg-red-500 p-1 text-white opacity-0 transition-opacity duration-200 group-hover:opacity-100"
                 >
                   <MdClose className="h-5 w-5" />
                 </button>
-                <div className="p-2">
-                  <p className="text-sm text-gray-500 truncate">
-                    {fileName}
-                  </p>
+                <div className="p-2 flex justify-between">
+                  <p className="truncate text-sm text-gray-500">{fileName}</p>
+                  <p className="text-sm text-gray-500">{((fileSize ?? 0) / 1024).toFixed(2)} KB</p>
                 </div>
               </div>
             </div>
           )}
-          {uploading && <Progress value={progress} className="w-full mb-4" />}
+          {uploading && <Progress value={progress} className="mb-4 w-full" />}
         </div>
         <DialogFooter>
           <button
             onClick={handleSubmit}
-            className="w-full bg-primary text-background hover:bg-primary-dark p-2 rounded"
+            className="hover:bg-primary-dark w-full rounded bg-primary p-2 text-background"
             disabled={uploading}
           >
-            {uploading ? "Subiendo..." : editingCourseId ? (isEditing ? "Editando..." : "Editar") : "Guardar"}
+            {uploading
+              ? "Subiendo..."
+              : editingCourseId
+                ? isEditing
+                  ? "Editando..."
+                  : "Editar"
+                : "Guardar"}
           </button>
         </DialogFooter>
       </DialogContent>
