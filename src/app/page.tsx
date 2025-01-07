ad089a1e26a6376f95e359871444930ce976098e
Artiefy <artiefy4@gmail.com>
2025-01-07 14:44:58 -0500

    spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { SignInButton, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import Link from "next/link";
+import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa"; // Importa el icono de flecha
 import SmoothGradient from "~/components/layout/Gradient";
 import { Header } from "~/components/layout/Header";
 import { Button as UiButton } from "~/components/ui/button";
-import { FaArrowRight } from "react-icons/fa"; // Importa el icono de flecha
+import { Icons } from "~/components/ui/icons"; // Importa el ícono de carga
 
 export default function Home() {
   const { user } = useUser();
+  const [loading, setLoading] = useState(false);
 
   // Determinar la ruta del dashboard según el rol del usuario
   const dashboardRoute =
@@ -17,19 +20,31 @@ export default function Home() {
         ? "/dashboard/profesores"
         : "/estudiantes"; // Ruta predeterminada para usuarios sin rol o estudiantes
 
+  const handleButtonClick = () => {
+    setLoading(true);
+  };
+
   return (
     <div className="relative flex min-h-screen flex-col">
       <SmoothGradient />
       <div className="relative z-10 flex min-h-screen flex-col">
         <Header />
-        <main className="flex flex-grow items-center justify-center mt-[-10vh]"> {/* Ajusta el margen superior */}
-          <section className="container mx-auto px-4 py-12 text-center"> {/* Ajusta el padding */}
-            <h1 className="mb-5 text-5xl font-bold leading-snug text-white"> {/* Ajusta el margen inferior */}
+        <main className="mt-[-10vh] flex flex-grow items-center justify-center">
+          {" "}
+          {/* Ajusta el margen superior */}
+          <section className="container mx-auto px-4 py-12 text-center">
+            {" "}
+            {/* Ajusta el padding */}
+            <h1 className="mb-5 text-5xl font-bold leading-snug text-white">
+              {" "}
+              {/* Ajusta el margen inferior */}
               Únete a nosotros y transforma tus ideas en
               <br /> realidades con el{" "}
               <span className="text-primary">poder del conocimiento</span>
             </h1>
-            <p className="mb-5 text-xl leading-snug"> {/* Ajusta el margen inferior */}
+            <p className="mb-5 text-xl leading-snug">
+              {" "}
+              {/* Ajusta el margen inferior */}
               Bienvenido a Artiefy, tu plataforma digital educativa dedicada a
               impulsar <br /> tus conociminetos con ciencia y tecnología.
             </p>
@@ -37,23 +52,60 @@ export default function Home() {
               <SignedOut>
                 <SignInButton>
                   <UiButton
-                    className="cta rounded-none relative p-7 text-2xl font-semibold italic text-background active:scale-95 bg-primary transform skew-x-[-20deg] hover:text-white"
+                    className="cta relative skew-x-[-20deg] transform rounded-none bg-primary p-7 text-2xl font-semibold italic text-background hover:text-white active:scale-95"
                     style={{
                       boxShadow: "6px 6px 0 black",
                       transition: "0.5s",
+                      width: "250px",
                     }}
+                    onClick={handleButtonClick}
                   >
-                    <span className="inline-block transform skew-x-[15deg]">COMIENZA YA</span>
-                    <FaArrowRight className="animate-bounce-right second inline-block transform skew-x-[15deg] ml-2 transition-transform duration-500" />
+                    <div className="flex items-center justify-center w-full">
+                      {loading ? (
+                        <Icons.spinner
+                          className="animate-spin"
+                          style={{ height: "32px", width: "32px" }} // Ajusta el tamaño del spinner
+                        />
+                      ) : (
+                        <>
+                          <span className="inline-block skew-x-[15deg] transform">
+                            COMIENZA YA
+                          </span>
+                          <FaArrowRight className="animate-bounce-right second ml-2 inline-block skew-x-[15deg] transform transition-transform duration-500" />
+                        </>
+                      )}
+                    </div>
                   </UiButton>
                 </SignInButton>
               </SignedOut>
               <SignedIn>
                 <UiButton
                   asChild
-                  className="p-5 text-2xl font-semibold active:scale-95"
+                  className="cta relative skew-x-[-20deg] transform rounded-none bg-primary p-7 text-2xl font-semibold italic text-background hover:text-white active:scale-95"
+                  style={{
+                    boxShadow: "6px 6px 0 black",
+                    transition: "0.5s",
+                    width: "250px",
+                  }}
+                  onClick={handleButtonClick}
                 >
-                  <Link href={dashboardRoute}>INICIO</Link>
+                  <Link href={dashboardRoute}>
+                    <div className="flex w-full items-center justify-center">
+                      {loading ? (
+                        <Icons.spinner
+                          className="animate-spin"
+                          style={{ height: "32px", width: "32px" }} 
+                        />
+                      ) : (
+                        <>
+                          <span className="inline-block skew-x-[15deg] transform">
+                            COMIENZA YA
+                          </span>
+                          <FaArrowRight className="animate-bounce-right second ml-2 inline-block skew-x-[15deg] transform transition-transform duration-500" />
+                        </>
+                      )}
+                    </div>
+                  </Link>
                 </UiButton>
               </SignedIn>
             </div>
