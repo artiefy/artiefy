e3171874481237e47f5cbc78e3731ba4f4ef4d45
Artiefy <artiefy4@gmail.com>
2025-01-15 20:03:32 -0500

    image

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,10 @@ const coreConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'inline',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 's3.us-east-2.amazonaws.com',
+        hostname: 's3.amazonaws.com',
         port: '',
         pathname: '/artiefy-upload/**',
       },
