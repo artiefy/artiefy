64a941fb7aca618cb93ba5812455ee2dcaf24692
Artiefy <artiefy4@gmail.com>
2025-01-29 02:25:59 -0500

    og

--- src/app/estudiantes/cursos/[id]/opengraph-image.tsx
+++ src/app/estudiantes/cursos/[id]/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from 'next/og';
 import { getCourseById } from '~/server/actions/courses/getCourseById';
 
-export const runtime = 'edge';
 
 export const alt = 'Course Image';
 export const size = {
