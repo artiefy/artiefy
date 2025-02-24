75ac848189393199c6b6b585aef7eec59cf02bc2
Artiefy <artiefy4@gmail.com>
2025-02-24 03:34:53 -0500

    get

--- src/app/api/cron/check-subscriptions/route.ts
+++ src/app/api/cron/check-subscriptions/route.ts
@@ -1,24 +1,23 @@
-import { type NextApiRequest, type NextApiResponse } from 'next';
+import { type NextRequest, NextResponse } from 'next/server';
 import { checkAndUpdateSubscriptions } from '~/server/actions/estudiantes/subscriptions/checkAndUpdateSubscriptions';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const authHeader = req.headers.authorization;
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
+export async function GET(request: NextRequest) {
+	const authHeader = request.headers.get('authorization');
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response('Unauthorized', {
+			status: 401,
+		});
+	}
 
-  try {
-    await checkAndUpdateSubscriptions();
-    res
-      .status(200)
-      .json({ message: 'Suscripciones verificadas y actualizadas' });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: 'Error al verificar y actualizar las suscripciones' });
-  }
+	try {
+		await checkAndUpdateSubscriptions();
+		return NextResponse.json({
+			message: 'Suscripciones verificadas y actualizadas',
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response('Error al verificar y actualizar las suscripciones', {
+			status: 500,
+		});
+	}
 }
