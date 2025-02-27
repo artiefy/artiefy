61ac67fc0322efcd2536e13e3cacac33d0d67a27
Artiefy <artiefy4@gmail.com>
2025-02-27 15:32:13 -0500

    loader spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,35 +1,36 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import '~/styles/rocket.css';
 import '~/styles/loading.css';
 
 const Loading: React.FC = () => {
-	const [launch, setLaunch] = useState(false);
+  const [launch, setLaunch] = useState(false);
 
-	useEffect(() => {
-		setLaunch(true);
-	}, []);
+  useEffect(() => {
+    setLaunch(true);
+  }, []);
 
-	return (
-		<div className="fullscreen-background">
-			<div className={`card ${launch ? 'launch' : ''}`}>
-				<div className="card-info">
-					<div className="loading-container">
-						<div className="loader" aria-live="assertive" role="alert"></div>
-					</div>
-					<span className="animate-pulse-text">CARGANDO!</span>
-				</div>
-				<Image
-					src="/cursor.png"
-					alt="Logo"
-					className="logo"
-					width={140}
-					height={140}
-					priority
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="fullscreen-background">
+      <div className={`card ${launch ? 'launch' : ''}`}>
+        <div className="card-info">
+          <div className="loading-container">
+            <div className="loader animate-spin" aria-live="assertive" role="alert"></div>
+          </div>
+          <span className="animate-pulse-text">CARGANDO!</span>
+        </div>
+        <Image
+          src="/cursor.png"
+          alt="Logo"
+          className="logo"
+          width={140}
+          height={140}
+          priority
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
