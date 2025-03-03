6873badf0b76f0d8eb1d53290d911f4b7b526d98
Artiefy <artiefy4@gmail.com>
2025-03-02 21:53:18 -0500

    sdds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,65 +12,65 @@ import Loading from './loading';
 import Providers from './providers';
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-montserrat',
-  preload: false,
-  weight: ['400', '500', '600', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-montserrat',
+	preload: false,
+	weight: ['400', '500', '600', '700'],
 });
 
 export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 };
 
 export const metadata: Metadata = siteMetadata;
 
 const jsonLd = {
-  '@context': 'https://schema.org',
-  '@type': 'WebSite',
-  url: 'https://artiefy.com/',
-  name: 'Artiefy',
-  description:
-    'Artiefy es la plataforma de aprendizaje más innovadora para estudiantes y profesores.',
-  logo: {
-    '@type': 'ImageObject',
-    url: 'https://artiefy.com/artiefy-icon.png',
-  },
+	'@context': 'https://schema.org',
+	'@type': 'WebSite',
+	url: 'https://artiefy.com/',
+	name: 'Artiefy',
+	description:
+		'Artiefy es la plataforma de aprendizaje más innovadora para estudiantes y profesores.',
+	logo: {
+		'@type': 'ImageObject',
+		url: 'https://artiefy.com/artiefy-icon.png',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider localization={esMX} dynamic>
-      <html lang="es" className={`${montserrat.variable}`}>
-        <head>
-          <Script
-            id="json-ld"
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify(jsonLd),
-            }}
-            strategy="afterInteractive"
-          />
-        </head>
-        <body className="bg-background font-sans text-primary">
-          <ClerkLoading>
-            <Loading />
-          </ClerkLoading>
-          <ClerkLoaded>
-            <Providers>{children}</Providers>
-          </ClerkLoaded>
-          <SpeedInsights />
-          <Analytics />
-          <Toaster />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider localization={esMX} dynamic>
+			<html lang="es" className={`${montserrat.variable}`}>
+				<head>
+					<Script
+						id="json-ld"
+						type="application/ld+json"
+						dangerouslySetInnerHTML={{
+							__html: JSON.stringify(jsonLd),
+						}}
+						strategy="afterInteractive"
+					/>
+				</head>
+				<body className="bg-background font-sans text-primary">
+					<ClerkLoading>
+						<Loading />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<Providers>{children}</Providers>
+					</ClerkLoaded>
+					<SpeedInsights />
+					<Analytics />
+					<Toaster />
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
