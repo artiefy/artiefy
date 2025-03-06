7cdf245edb303ef75a45b45ddcdf74b9fb69220b
Artiefy <artiefy4@gmail.com>
2025-03-06 01:27:56 -0500

    cripto ks

--- src/utils/paygateway/signature.ts
+++ src/utils/paygateway/signature.ts
@@ -1,13 +1,12 @@
-import CryptoJS from 'crypto-js';
+import crypto from 'crypto';
 
-// Función para calcular el hash MD5
 export function calculateMD5(
 	apiKey: string,
 	merchantId: string,
 	referenceCode: string,
 	amount: string,
 	currency: string
-): string {
-	const data = [apiKey, merchantId, referenceCode, amount, currency].join('~');
-	return CryptoJS.MD5(data).toString();
+) {
+	const data = `${apiKey}~${merchantId}~${referenceCode}~${amount}~${currency}`;
+	return crypto.createHash('md5').update(data).digest('hex');
 }
