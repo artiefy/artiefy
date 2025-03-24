aaf314784a4c176b03bf390720373ae4ae826050
Artiefy <artiefy4@gmail.com>
2025-03-23 19:31:25 -0500

    full robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,31 +4,20 @@ export default function robots(): MetadataRoute.Robots {
 	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://artiefy.com';
 
 	return {
-		rules: [
-			{
-				userAgent: '*',
-				allow: [
-					'/',
-					'/estudiantes',
-					'/planes',
-					'/estudiantes/cursos/*',
-					'/estudiantes/programas/*',
-				],
-				disallow: [
-					'/api/*',
-					'/server/*',
-					'/dashboard/*',
-					'/_next/*',
-					'/*.json$',
-					'/sign-in/*',
-					'/sign-up/*',
-					'/user-profile/*',
-					'/testing/*',
-					'/consult/*',
-					'/*?*', // Prevent crawling of URL parameters
-				],
-			},
-		],
+		rules: {
+			userAgent: '*',
+			allow: ['/'],
+			disallow: [
+				'/api/',
+				'/server/',
+				'/dashboard/',
+				'/sign-in/',
+				'/sign-up/',
+				'/user-profile/',
+				'/_next/',
+				'/*.json$',
+			],
+		},
 		sitemap: `${baseUrl}/sitemap.xml`,
 		host: baseUrl,
 	};
