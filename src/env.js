e7989015681d64a1592f9218c89b82cf7d6356a6
Artiefy <artiefy4@gmail.com>
2025-04-02 01:06:32 -0500

    pass

--- src/env.js
+++ src/env.js
@@ -103,7 +103,7 @@ export const env = createEnv({
 		SMTP_PORT: process.env.SMTP_PORT,
 		SMTP_USER: process.env.SMTP_USER,
 		SMTP_PASS: process.env.SMTP_PASS,
-		SMTP_GMAIL_PASS: process.env.SMTP_GMAIL_PASS,
+		PASS: process.env.PASS,
 
 		// App Config
 		NODE_ENV: process.env.NODE_ENV,
