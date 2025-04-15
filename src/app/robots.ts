69d4e3f646123f641eceba4622a39949bffb2fdb
artiefy <artiefy4@gmail.com>
2025-04-14 21:06:30 -0500

    robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,25 +1,12 @@
-import { type MetadataRoute } from 'next';
-
+import type { MetadataRoute } from 'next'
+ 
 export default function robots(): MetadataRoute.Robots {
-	return {
-		rules: [
-			{
-				userAgent: '*',
-				allow: '/',
-				disallow: [
-					'/loading/',
-					'/_next/',
-					'/api/',
-					'/dashboard/',
-					'/sign-in/',
-					'/sign-up/',
-					'/*.json$',
-					'*?loading=*',
-					'*loading*',
-				],
-			},
-		],
-		sitemap: 'https://artiefy.com/sitemap.xml',
-		host: 'https://artiefy.com',
-	};
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://artiefy.com/sitemap.xml',
+  }
 }
