35941b0ad6114d19e3d7a2de0fa1040c0349c31d
artiefy <artiefy4@gmail.com>
2025-05-22 14:29:57 -0500

    fix code

--- src/app/estudiantes/layout.tsx
+++ src/app/estudiantes/layout.tsx
@@ -17,5 +17,12 @@ export default function StudentLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	return <section className="relative min-h-screen">{children}</section>;
+	return (
+		<div className="relative min-h-screen">
+			{children}
+			<div className="fixed right-0 bottom-0 z-50">
+				<TicketSupportWrapper />
+			</div>
+		</div>
+	);
 }
