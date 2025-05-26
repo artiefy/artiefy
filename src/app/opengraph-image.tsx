9f2100d7cdaac9761a26a8a3bca0c9f67a3d4c48
artiefy <artiefy4@gmail.com>
2025-05-26 13:55:52 -0500

    icon metadata apple y demas

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export const alt = 'My Open Graph Image';
+export const alt = 'Artiefy - Plataforma Educativa Digital';
 export const size = {
 	width: 1200,
 	height: 630,
