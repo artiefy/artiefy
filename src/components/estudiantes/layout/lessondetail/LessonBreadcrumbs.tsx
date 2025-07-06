8408cf7b2ba75f8631a04f16619b69f0f9c245f8
techotaku1 <jsdg1818@gmail.com>
2025-07-06 06:49:33 -0500

    readcumbs

--- src/components/estudiantes/layout/lessondetail/LessonBreadcrumbs.tsx
+++ src/components/estudiantes/layout/lessondetail/LessonBreadcrumbs.tsx
@@ -19,7 +19,7 @@ const LessonBreadcrumbs = ({
 }: LessonBreadcrumbsProps) => {
   return (
     <nav className="w-full max-w-full overflow-x-auto px-2 md:px-8">
-      <ol className="flex items-center space-x-2 pr-2 text-sm whitespace-nowrap text-gray-400">
+      <ol className="flex flex-wrap items-center space-y-2 space-x-2 pr-2 text-sm whitespace-nowrap text-gray-400 sm:space-y-0">
         <li>
           <Link
             href="/"
@@ -45,20 +45,22 @@ const LessonBreadcrumbs = ({
         <li>
           <Link
             href={`/estudiantes/cursos/${courseId}`}
-            className="hover:text-primary flex max-w-[120px] items-center truncate hover:underline focus:ring-0 focus:outline-none md:max-w-xs"
+            className="hover:text-primary flex items-center break-words whitespace-normal hover:underline focus:ring-0 focus:outline-none"
             title={courseTitle}
           >
-            <FaBook className="mr-1" /> {courseTitle}
+            <FaBook className="mr-1" />{' '}
+            <span className="break-words whitespace-normal">{courseTitle}</span>
           </Link>
         </li>
         <li>
           <ChevronRightIcon className="h-4 w-4" />
         </li>
         <li
-          className="text-primary flex max-w-[120px] items-center truncate pr-2 font-bold md:max-w-xs"
+          className="text-primary mb-2 sm:mb-0 flex items-center pr-2 font-bold break-words whitespace-normal"
           title={lessonTitle}
         >
-          <SquarePlay className="mr-1 size-5" /> {lessonTitle}
+          <SquarePlay className="mr-1 size-5" />{' '}
+          <span className="break-words whitespace-normal">{lessonTitle}</span>
         </li>
       </ol>
     </nav>
